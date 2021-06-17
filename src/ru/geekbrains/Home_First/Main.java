e6aa351224c3994afee2f1f8045e7e8a6450ca9c
geekbrains <info@geekbrains.ru>
2021-06-17 13:33:46 -0700

    2_Exersise

--- src/ru/geekbrains/Home_First/Main.java
+++ src/ru/geekbrains/Home_First/Main.java
@@ -3,6 +3,11 @@ package ru.geekbrains.Home_First;
 public class Main {
 
     public static void main(String[] args) {
-	
+        printThreeWords();
+    }
+    public static void printThreeWords() {
+        System.out.println("_Orange");
+        System.out.println("_ Banana");
+        System.out.println("_ Apple");
     }
 }
