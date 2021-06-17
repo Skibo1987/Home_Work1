b8b07ba9e481cc4c1b8943c1e6d5d81e92ce401a
geekbrains <info@geekbrains.ru>
2021-06-17 14:03:40 -0700

    2_Exersise_all

--- src/ru/geekbrains/Home_First/Main.java
+++ src/ru/geekbrains/Home_First/Main.java
@@ -4,10 +4,44 @@ public class Main {
 
     public static void main(String[] args) {
         printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
     }
     public static void printThreeWords() {
         System.out.println("_Orange");
         System.out.println("_ Banana");
         System.out.println("_ Apple");
+    }
+    public static void checkSumSign(){
+        int a=-10;
+        int b=50;
+        int c=a+b;
+        if(c>=0){
+            System.out.println("Сумма положительная");
+        }else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+    public static void printColor(){
+        int value=110;
+        if(value<=0){
+            System.out.println("Красный");
+        }
+        if(value>0 && value<=100){
+            System.out.println("Желтый");
+        }
+        if (value>100){
+            System.out.println("Зеленый");
+        }
+    }
+    public static void compareNumbers(){
+        int a=24;
+        int b=23;
+        if (a>=b){
+            System.out.println("a>=b");
+        }else {
+            System.out.println("a<b");
+        }
     }
 }
